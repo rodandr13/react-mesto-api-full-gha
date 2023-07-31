f82fa599bf0422fb6dd934aa4a5638fa3bb3968e
Andrey Roditelev <rodandr13@gmail.com>
2023-07-31 14:23:22 +0300

    fix: allowedcors

--- backend/utils/constans.js
+++ backend/utils/constans.js
@@ -11,6 +11,8 @@ const allowedCors = [
   'http://localhost',
   'localhost',
   'https://localhost:3000',
+  'http://127.0.0.1:3000',
+  'http://127.0.0.1',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
